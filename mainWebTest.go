7e23f757198359a06ca511dc36e21736eabad6bf
Adrian Lorentzen <Adrian Lorentzen>
2017-03-28 20:10:39 +0200

    abra kadabra

--- mainWebTest.go
+++ mainWebTest.go
@@ -4,30 +4,57 @@ import (
 	"html/template"
 	"net/http"
 	"path"
-	"encoding/json"
-	"fmt"
 
+	"fmt"
+	"./decoders"
+	"log"
+	"io/ioutil"
+
+	"bytes"
+	"net"
 )
 
-type Profile struct {
-	Name    string
-	Hobbies []string
+//var URLS = make([]string, 3)
+var URLS = map[string]string{
+	"OWL" : "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1",
+	"Google" : "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDhdQvs9XLKd7TVYyYX98WWfB1z4VOddko",
+
 }
+var IPaddr string
 
 func main() {
+	IPaddr = GetLocalIP()
+	fmt.Println(IPaddr)
 	//http.HandleFunc("/search", search)
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/search", foo2)
+	//URLS[0] = "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1"
+	//URLS[1] = "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1"
+	//URLS[2] = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDhdQvs9XLKd7TVYyYX98WWfB1z4VOddko"
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/search", searchBox)
 	http.HandleFunc("/AltSubmit", formInputHandler)
-	http.HandleFunc("/ttt", foo2)
-	http.HandleFunc("/ddd", foo2)
-	http.HandleFunc("/fff", foo2)
+	//http.HandleFunc("/ttt", searchBox)
+	//http.HandleFunc("/ddd", searchBox)
+	//http.HandleFunc("/fff", searchBox)
 	http.ListenAndServe(":8001", nil)
-	//go http.HandleFunc("/", foo2)
+	//go http.HandleFunc("/", searchBox)
+}
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+func homepage(w http.ResponseWriter, r *http.Request) {
 
 	lp := path.Join("templates", "layout.html")
 	fp := path.Join("templates", "indexTest.html")
@@ -39,29 +66,91 @@ func foo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := tmpl.Execute(w, profile); err != nil {
+	if err := tmpl.Execute(w, "test"); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
 }
-func foo2(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
-	r.ParseForm()  //Parse url parameters passed, then parse the response packet for the POST body (request body)
+
+func searchBox(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
 	// attention: If you do not call ParseForm method, the following data can not be obtained form
 	fmt.Println(r.Form) // print information on server side.
 	name := r.Form.Get("name")
 	fmt.Println(name)
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if name == "all" {
+		//for i := 0; i < len(URLS); i++ {
+		//go doGet(URLS)
+		for key := range URLS {
+			if key == "Google" {
+				i := URLS[key]
+				go getGoogle(i)
+			} else if key == "OWL" {
+				i := URLS[key]
+				go doGet(i)
+			}
+
+		}
+
 	}
-
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(js)
 }
+func doGet(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("The calculated length is:", len(string(contents)), "for the url:", url)
+		fmt.Println(" ", response.StatusCode)
+		hdr := response.Header
+		for key, value := range hdr {
+			fmt.Println(" ", key, ":", value,)
+
+		}
+		fmt.Printf("%q", contents)
+
+		decoders.DecodeOWL(contents)
+		//response.Header.Set("Content-Type", "application/json")
+
+
+		//go DecodeOWL(js)
+	}
+}
+func getGoogle(url string) {
+	//response, err := http.Get(url)
+
+	// handle err
+	var jsonStr = []byte(`{
+  "macAddress": "00:25:9c:cf:1c:ac",
+  "signalStrength": -43,
+  "age": 0,
+  "channel": 11,
+  "signalToNoiseRatio": 0
+}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	//req.Header.Set("X-Custom-Header", "myvalue")
+	//req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+	fmt.Printf("%q", body)
+
+	go decoders.GogleDecoder(body)
+}
+
+
 
 func formInputHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()  //Parse url parameters passed, then parse the response packet for the POST body (request body)
@@ -80,32 +169,4 @@ func formInputHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(k)
 		}
 	}
-}
-
-func foo4(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
-
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(js)
-}
-
-func foo5(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
-
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(js)
 }
