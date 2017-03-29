8be32d5f80f3dd8a23e22f0a61d50b2ad3456b2f
ampede15 <ampede15@uia.no>
2017-03-29 20:59:35 +0200

    fungerende pokeapi

---
+++ decoders/decoderBf4stats.go
@@ -0,0 +1,55 @@
+package decoders
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+)
+
+type OnlinePlayers struct {
+	Label  string `json:"pc.label"`
+	Count  float64
+	Peak24 float64
+}
+
+func DecodeOnlinePlayers(test []byte) {
+	//fmt.Printf("q", test)
+	// Her brukes det kun et utdrag fra data som var i responsen fra OWL
+	// For å bruke strøm fra doGet funksjonen, må hele JSON-strukturen
+	// defineres; kun Coordinates og Measurements (main) er definert i
+	// dette eksemplet
+
+	// Definerer en struktur i Golang etter strukturen fra API-en (openweather)
+	// Her kan man virkelig se “styrken” av Golangs struct
+	// Datafelt i struct må være med en storbokstav og navn må tilsvare
+	// de navn som er i jsonStream (de kan begynne med små bokstaver)
+
+	// Ting er strøm-basert, som vi har snakket om tidligere
+	dec := json.NewDecoder(bytes.NewReader(test))
+	for {
+		// Definerer struktur for en instans av Weather strukturen
+		// Dette avhenger selvfølgelig om hva som returneres fra
+		// webtjenesten (openweather i dette tilfelle)
+		var c OnlinePlayers
+		//var m Measurements
+		// Passerer adressen til Weather-strukturen w til funksjonen
+		// Decode (som kalles fra en json.NewDecoder med
+		// strings.NewReader(jsonStream) som IN-DATA-STRØM
+		// Når det ikke er mer data (EOF) bryter vi utførelsen av
+		// denne funksjonen med break
+		if err := dec.Decode(&c); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		// Her er et par eksempler på hvordan man kan skrive ut
+		// data fra denne webtjenesten på en brukbar måte
+		// Dette er noe dere skal prøve å imitere med data
+		// fra andre webtjenester (med andre API-er, selvsagt)
+		fmt.Println("\n Country:\n",
+			c.Label, c.Count, c.Peak24)
+
+	}
+}
